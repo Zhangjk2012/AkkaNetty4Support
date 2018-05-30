fec29e3d3c0bf077756bd6078039933a6dbfe984
zhangjiangke <zhangjiangke@baidu.com>
2018-05-30 18:37:13 +0800

    格式化

--- src/main/scala/akka/remote/transport/netty4/NettyTransport.scala
+++ src/main/scala/akka/remote/transport/netty4/NettyTransport.scala
@@ -37,14 +37,19 @@ private[transport] object NettyTransport {
 
   val isUseEpoll = OS_NAME match {
     case "" => false
-    case _ if (Helpers.toRootLowerCase(OS_NAME).indexOf("linux") >= 0 && Epoll.isAvailable())  => true
+    case _ if (Helpers.toRootLowerCase(OS_NAME).indexOf("linux") >= 0 && Epoll.isAvailable()) => true
   }
 
   def gracefulClose(channel: Channel)(implicit ec: ExecutionContext): Unit = {
     def always(c: ChannelFuture) = channelFuture2ScalaFuture(c) recover { case _ ⇒ c.channel() }
+
     for {
-      _ ← always { channel.write(Unpooled.buffer(0)) } // Force flush by waiting on a final dummy write
-      _ ← always { channel.disconnect() }
+      _ ← always {
+        channel.write(Unpooled.buffer(0))
+      } // Force flush by waiting on a final dummy write
+      _ ← always {
+        channel.disconnect()
+      }
     } channel.close()
   }
 
@@ -64,7 +69,7 @@ private[transport] object NettyTransport {
 
   def channelFuture2ScalaFuture(channelFuture: ChannelFuture): Future[Channel] = {
     val p = Promise[Channel]()
-    channelFuture.addListener(new ChannelFutureListener(){
+    channelFuture.addListener(new ChannelFutureListener() {
       override def operationComplete(future: ChannelFuture): Unit = p complete Try {
         if (future.isSuccess) future.channel()
         else if (future.isCancelled) throw new CancellationException
@@ -77,12 +82,12 @@ private[transport] object NettyTransport {
   def channelGroupFuture2ScalaFuture(channelFuture: ChannelGroupFuture) = {
     val p = Promise[ChannelGroup]()
     import scala.collection.JavaConverters._
-    channelFuture.addListener(new ChannelGroupFutureListener(){
+    channelFuture.addListener(new ChannelGroupFutureListener() {
       override def operationComplete(future: ChannelGroupFuture): Unit = p complete Try {
         if (future.isSuccess) future.group
         else throw future.iterator.asScala.collectFirst {
           case f if f.isCancelled ⇒ new CancellationException
-          case f if !f.isSuccess  ⇒ f.cause()
+          case f if !f.isSuccess ⇒ f.cause()
         } getOrElse new IllegalStateException("Error reported in ChannelGroupFuture, but no error found in individual futures.")
       }
     })
@@ -91,8 +96,9 @@ private[transport] object NettyTransport {
 
   /**
     * create event loop group
-    * @param threadNum      thread numbers
-    * @param threadFactory  thread factory
+    *
+    * @param threadNum     thread numbers
+    * @param threadFactory thread factory
     * @return
     */
   def createEventLoopGroup(threadNum: Int, threadFactory: ThreadFactory): EventLoopGroup = {
@@ -106,6 +112,7 @@ private[transport] object NettyTransport {
 
   /**
     * create a named thread factory.
+    *
     * @param threadNum
     * @param threadName
     * @param daemon
@@ -113,8 +120,11 @@ private[transport] object NettyTransport {
     */
   def namedThreadFactory(threadNum: Int, threadName: String, daemon: Boolean): ThreadFactory = {
     val threadFactory = new ThreadFactory {
+
       import java.util.concurrent.atomic.AtomicInteger
+
       val threadIndex = new AtomicInteger(0)
+
       override def newThread(r: Runnable): Thread = {
         val thread = new Thread(r, s"${threadName}_${threadNum}_${threadIndex.getAndIncrement()}")
         thread.setDaemon(daemon)
@@ -126,13 +136,15 @@ private[transport] object NettyTransport {
 
 }
 
-class Netty4TransportSettings(config: Config) extends NettyTransportSettings(config){
+class Netty4TransportSettings(config: Config) extends NettyTransportSettings(config) {
+
   import config._
+
   val ServerSocketThreadName = getString("server-socket-pool-name")
   val ClientSocketThreadName = getString("client-socket-pool-name")
 }
 
-class NettyTransport (val settings: Netty4TransportSettings, val system: ExtendedActorSystem) extends Transport{
+class NettyTransport(val settings: Netty4TransportSettings, val system: ExtendedActorSystem) extends Transport {
 
   def this(system: ExtendedActorSystem, conf: Config) = this(new Netty4TransportSettings(conf), system)
 
@@ -141,14 +153,14 @@ class NettyTransport (val settings: Netty4TransportSettings, val system: Extende
 
   implicit val executionContext: ExecutionContext =
     settings.UseDispatcherForIo.orElse(RARP(system).provider.remoteSettings.Dispatcher match {
-      case ""             ⇒ None
+      case "" ⇒ None
       case dispatcherName ⇒ Some(dispatcherName)
     }).map(system.dispatchers.lookup).getOrElse(system.dispatcher)
 
   val bossEventLoopGroup = createEventLoopGroup(1,
-        namedThreadFactory(1, s"${ServerSocketThreadName}_Boss", true))
+    namedThreadFactory(1, s"${ServerSocketThreadName}_Boss", true))
   val workEventLoopGroup = createEventLoopGroup(ServerSocketWorkerPoolSize,
-        namedThreadFactory(ServerSocketWorkerPoolSize, s"${ServerSocketThreadName}_Worker", false))
+    namedThreadFactory(ServerSocketWorkerPoolSize, s"${ServerSocketThreadName}_Worker", false))
 
   /** 存放所有Server端连接的channel */
   val channelGroup = new DefaultChannelGroup("akka-netty-transport-driver-channelgroup-" +
@@ -167,7 +179,7 @@ class NettyTransport (val settings: Netty4TransportSettings, val system: Extende
       val b = new ServerBootstrap()
       b.group(bossEventLoopGroup, workEventLoopGroup)
       isUseEpoll match {
-        case true  => b.channel(classOf[EpollServerSocketChannel])
+        case true => b.channel(classOf[EpollServerSocketChannel])
         case false => b.channel(classOf[NioServerSocketChannel])
       }
       b.option[Integer](ChannelOption.SO_BACKLOG, Backlog)
@@ -179,7 +191,7 @@ class NettyTransport (val settings: Netty4TransportSettings, val system: Extende
       WriteBufferHighWaterMark.flatMap(high => WriteBufferLowWaterMark.map(low => (low, high))).foreach(o => {
         b.childOption(ChannelOption.WRITE_BUFFER_WATER_MARK, new WriteBufferWaterMark(o._1, o._2))
       })
-      b.childHandler(new ChannelInitializer[SocketChannel](){
+      b.childHandler(new ChannelInitializer[SocketChannel]() {
         override def initChannel(ch: SocketChannel): Unit = {
           val pipeline = ch.pipeline()
           pipeline.addLast("FrameDecoder", new LengthFieldBasedFrameDecoder(
@@ -216,8 +228,12 @@ class NettyTransport (val settings: Netty4TransportSettings, val system: Extende
 
   // TODO: This should be factored out to an async (or thread-isolated) name lookup service #2960
   def addressToSocketAddress(address: Address): Future[InetSocketAddress] = address match {
-    case Address(_, _, Some(host), Some(port)) ⇒ Future { blocking { new InetSocketAddress(InetAddress.getByName(host), port) } }
-    case _                                     ⇒ Future.failed(new IllegalArgumentException(s"Address [$address] does not contain host or port information."))
+    case Address(_, _, Some(host), Some(port)) ⇒ Future {
+      blocking {
+        new InetSocketAddress(InetAddress.getByName(host), port)
+      }
+    }
+    case _ ⇒ Future.failed(new IllegalArgumentException(s"Address [$address] does not contain host or port information."))
   }
 
   // 配置监听事件
@@ -228,7 +244,7 @@ class NettyTransport (val settings: Netty4TransportSettings, val system: Extende
     } yield {
       try {
         val newServerChannel = inboundBootstrap match {
-          case b: ServerBootstrap         ⇒ b.bind(address).channel()
+          case b: ServerBootstrap ⇒ b.bind(address).channel()
           //case b: ConnectionlessBootstrap ⇒ b.bind(address)
         }
 
@@ -245,21 +261,30 @@ class NettyTransport (val settings: Netty4TransportSettings, val system: Extende
           case Some(address) ⇒
             addressFromSocketAddress(newServerChannel.localAddress(), schemeIdentifier, system.name, None, None) match {
               case Some(address) ⇒ boundTo = address
-              case None          ⇒ throw new NettyTransportException(s"Unknown local address type [${newServerChannel.getLocalAddress.getClass.getName}]")
+              case None ⇒
+                throw new NettyTransportException(s"Unknown local address type [${newServerChannel.localAddress().getClass.getName}]")
             }
             localAddress = address
             associationListenerPromise.future.foreach { _ ⇒
+              // 外面Manager注册成功，设置autoRead为true。可以接收客户端的连接
+              // 这个Manager，可以通过上面的_获取，因为，它是一个listener.
+              // AkkaProtocolManager，这个manager，用来当有client时，进行的操作等等。
+              // 最终，在这个manager中会持有EndpointManager这个actor，保存到associationListener变量中。
+              // 到这里，就不需要再关心外部初始化了，这里已经完成了netty4的变更，需要额外的工作是，处理handler，sslHandler
               newServerChannel.config.setAutoRead(true)
-              //fixme 是否需要手动触发读取事件，需要测试
-              // newServerChannel.read()
             }
             (address, associationListenerPromise)
-          case None ⇒ throw new NettyTransportException(s"Unknown local address type [${newServerChannel.localAddress().getClass.getName}]")
+          case None ⇒
+            throw new NettyTransportException(s"Unknown local address type [${newServerChannel.localAddress().getClass.getName}]")
         }
       } catch {
         case NonFatal(e) ⇒ {
           log.error("failed to bind to {}, shutting down Netty transport", address)
-          try { shutdown() } catch { case NonFatal(e) ⇒ } // ignore possible exception during shutdown
+          try {
+            shutdown()
+          } catch {
+            case NonFatal(_) ⇒
+          } // ignore possible exception during shutdown
           throw e
         }
       }
